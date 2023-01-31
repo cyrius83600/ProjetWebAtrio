ad24298248804eb4390ac457e719dd5a894a6e15
Cyril CONCAS <concascyril2@hotmail.com>
2023-01-31 18:01:24 +0100

    Script SQL

---
+++ InitialisationBase.sql
@@ -0,0 +1,25 @@
+USE [TestTechnique]
+GO
+
+INSERT INTO Personnes(Nom, Prenom, DateNaissance)
+     VALUES('CONCAS', 'Cyril', '1998-02-01');
+INSERT INTO Personnes(Nom, Prenom, DateNaissance)
+     VALUES('DUPONT', 'Laurent', '1985-12-21');
+INSERT INTO Personnes(Nom, Prenom, DateNaissance)
+     VALUES('HIDALGO', 'Anne', '1952-05-17');
+INSERT INTO Personnes(Nom, Prenom, DateNaissance)
+     VALUES('CHIRAC', 'Jacques', '1930-12-31');
+INSERT INTO Personnes(Nom, Prenom, DateNaissance)
+     VALUES('PLATINI', 'Michelle', '1990-09-15');
+
+INSERT INTO Emplois(Entreprise, Poste)
+	 VALUES('EBP', 'Developpeur')
+INSERT INTO Emplois(Entreprise, Poste)
+	 VALUES('EBP', 'PO')
+INSERT INTO Emplois(Entreprise, Poste)
+	 VALUES('EBP', 'Scrum Master')
+INSERT INTO Emplois(Entreprise, Poste)
+	 VALUES('Fidesio', 'Chef de projet')
+GO
+
+
